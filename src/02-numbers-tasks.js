32ed9ee815e2e1e675b018c3a5eda23b87e351da
ElizavetaRazumenko <razumenko99@mail.ru>
2023-06-12 22:01:59 +0300

    feat: finish numbers tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -115,8 +115,16 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  // throw new Error('Not implemented');
+  let angl = Math.atan2(x1, y1) - Math.atan2(x2, y2);
+  if (angl < 0) {
+    angl += 2 * Math.PI;
+  }
+  if (angl > Math.PI) {
+    angl -= 2 * Math.PI;
+  }
+  return angl;
 }
 
 /**
@@ -166,8 +174,9 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  // throw new Error('Not implemented');
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 
@@ -188,8 +197,9 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  // throw new Error('Not implemented');
+  return Math.round(num / 10 ** pow) * (10 ** pow);
 }
 
 /**
@@ -209,8 +219,14 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  // throw new Error('Not implemented');
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0 || n === 1) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -228,8 +244,10 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  // throw new Error('Not implemented');
+  const number = +value;
+  return Number(number) || def;
 }
 
 module.exports = {
